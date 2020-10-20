6037414ac2a4d9d36fe7fc33068c13814494a4ee
Beijinho <60109149+Beijinho@users.noreply.github.com>
2020-10-20 09:00:07 +0100

    Vent working
    Player can now enter the vent and travel inside it

--- BingerNinja/Assets/Scripts/StealthObjects/StealthObject_JoaoBeijinho.cs
+++ BingerNinja/Assets/Scripts/StealthObjects/StealthObject_JoaoBeijinho.cs
@@ -2,6 +2,7 @@
 ///This class handles interactions with stealth objects
 
 //Joao Beijinho 18/10/2020 - Hide function and variables to find stealth objects scripts aswell as the player stealth script
+//Joao Beijinho 20/10/2020 - Implemented trigger for entering and exiting the vent
 
 using System.Collections;
 using System.Collections.Generic;
@@ -10,25 +11,39 @@ using UnityEngine;
 public class StealthObject_JoaoBeijinho : MonoBehaviour
 {
     PlayerStealth_JoaoBeijinho playerStealthScript;
-    Vent_JoaoBeijinho ventScript;
+    public Vent_JoaoBeijinho ventScript;
 
     public void Hide()
     {
         playerStealthScript.m_stealthed = !playerStealthScript.m_stealthed;
     }
-    
+
+    #region Enter and Exit triggers
+    private void OnTriggerEnter2D(Collider2D collision)//Enter and Exit vent
+    {
+        if (collision.tag == "Player")
+        {
+            if (playerStealthScript.m_stealthed == false)//If player isn't stealthed he's not inside the vent
+            {
+                ventScript.VentEnter();
+            }
+            else
+            {
+                ventScript.VentExit();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+    #endregion
+
     // Start is called before the first frame update
     private void Start()
     {
         playerStealthScript = FindObjectOfType<PlayerStealth_JoaoBeijinho>();
-        ventScript = FindObjectOfType<Vent_JoaoBeijinho>();
+        ventScript = gameObject.GetComponent<Vent_JoaoBeijinho>();
     }
-
-
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //
-    //}
 }
--- BingerNinja/Assets/Scripts/StealthObjects/Vent_JoaoBeijinho.cs
+++ BingerNinja/Assets/Scripts/StealthObjects/Vent_JoaoBeijinho.cs
@@ -2,6 +2,7 @@
 ///This class makes the player enter and exit vents aswell as restrict the player movement inside the vent
 
 //Joao Beijinho 19/10/2020 - Created draft of the enter and exit states for the vents
+//Joao Beijinho 20/10/2020 - Implemented vent walls and vent path. Moved trigger events to StealthObject
 
 using System.Collections;
 using System.Collections.Generic;
@@ -11,52 +12,52 @@ public class Vent_JoaoBeijinho : MonoBehaviour
 {
     StealthObject_JoaoBeijinho steathObjectScript;
 
-    GameObject player;
-    GameObject ventPath;
+    private GameObject m_player;
+    private GameObject m_ventPath;
+    private GameObject m_walls;
 
-    //Vents and player position to make player enter vents
-    private Transform playerPos;
-    private Transform ventPos;
-    private Transform ventPathPos;
+    //Player and wall tiles collider
+    private BoxCollider2D m_playerCollider;
+    private BoxCollider2D[] m_ventWallsCollider;
 
-    #region Enter and Exit triggers
-    private void OnTriggerEnter2D(Collider2D collision)//Vent Enter
-    {
-        if (collision.tag == "Player")
-        {
+    private Collider2D m_wallsCollider;
 
-            VentEnter();
+    //Turn vent walls on/off
+    private bool m_enableVentWallCollision = false;
 
-            print("vent enter");
-        }
-    }
+    //Player and vents position to snap player when entering or exiting vent
+    private Transform m_playerPos;
+    private Transform m_ventPos;
 
-    private void OnTriggerExit2D(Collider2D collision)//Vent Exit
-    {
-        if (collision.tag == "Player")
-        {
-
-            VentExit();
-
-            print("vent exit");
-        }
-    }
-    #endregion
-
-
-    //On enter contact place player on the position of the vent but on the z position of the vent path
-    //Change to be made - place player on vent position, disable any collisions except vent collisions
-    private void VentEnter()
+    //On enter contact snap player to the position of the vent, disable collision with wall tiles
+    public void VentEnter()
     {
         steathObjectScript.Hide();
-        playerPos.position = new Vector3(ventPos.position.x, ventPos.position.y, ventPathPos.position.z);
+
+        m_playerPos.position = new Vector3(m_ventPos.position.x, m_ventPos.position.y, m_ventPos.position.z);
+
+        foreach (BoxCollider2D ventWall in m_ventWallsCollider)
+        {
+            ventWall.enabled = !m_enableVentWallCollision;
+        }
+
+        Physics2D.IgnoreCollision(m_playerCollider, m_wallsCollider);
     }
      
-    //On exit contact place player on the position of the vent and back on the z position of the vent
-    private void VentExit()
+    //On exit contact snap player to the position of the vent, enable collision with wall tiles
+    public void VentExit()
     {
         steathObjectScript.Hide();
-    }
+
+        m_playerPos.position = new Vector2(m_ventPos.position.x, m_ventPos.position.y);
+
+        foreach (BoxCollider2D ventWall in m_ventWallsCollider)
+        {
+            ventWall.enabled = false;
+        }
+
+        Physics2D.IgnoreCollision(m_playerCollider, m_wallsCollider, false);
+    } 
 
     // Start is called before the first frame update
     void Start()
@@ -64,20 +65,17 @@ public class Vent_JoaoBeijinho : MonoBehaviour
         steathObjectScript = FindObjectOfType<StealthObject_JoaoBeijinho>();
 
         //Get the player, its position and its collider
-        player = GameObject.Find("Player");
-        playerPos = player.GetComponent<Transform>();
+        m_player = GameObject.Find("Player");
+        m_playerPos = m_player.GetComponent<Transform>();
+        m_playerCollider = m_player.GetComponent <BoxCollider2D>();
 
+        //Get the wall tiles and their collider
+        m_walls = GameObject.Find("Wall Tiles");
+        m_wallsCollider = m_walls.GetComponent<Collider2D>();
 
-        //Get the vent, vent path and their position
-        ventPos = gameObject.GetComponent<Transform>();
-        ventPath = GameObject.Find("VentPath");
-        ventPathPos = ventPath.GetComponent<Transform>();
+        //Get the vent, vent path and the vent walls collider
+        m_ventPos = gameObject.GetComponent<Transform>();
+        m_ventPath = GameObject.Find("VentPath");
+        m_ventWallsCollider = m_ventPath.GetComponentsInChildren<BoxCollider2D>();
     }
-
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    
-    //}
 }
