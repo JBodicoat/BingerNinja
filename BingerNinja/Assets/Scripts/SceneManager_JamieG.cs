255721809499a3b84c159cd729c861e590778fa1
JamGrif <40271350+JamGrif@users.noreply.github.com>
2020-10-26 17:14:59 +0000

    SceneManager change

--- BingerNinja/Assets/Scripts/SceneManager_JamieG.cs
+++ BingerNinja/Assets/Scripts/SceneManager_JamieG.cs
@@ -1,34 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//Jamie - Functions used to load different scenes
+
+//Jamie - 26/10/20 - First implemented
+///IMPORTANT - Correct scene order must be setup in projects build settings scene index
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager_JamieG : MonoBehaviour
+public static class SceneManager_JamieG 
 {
-    void Start()
-    {
-        //Load MainMenu on project startup
-        LoadMainMenu();
-    }
-
     //This will assume that the MainMenu scene is build index 0
-    public void LoadMainMenu()
+    public static void LoadMainMenu()
     {
         if (SceneManager.GetActiveScene().buildIndex != 0)
         {
             //SceneManager.LoadScene(0); // - Commented out for the moment until the the build settings index order is correctly setup
         }
-
     }
 
     //Assumes that the scenes are in the correct order of build indexes in build settings
-    public void LoadNextLevel()
+    public static void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     //Reloads the current scene using its buildIndex
-    public void LoadCurrentLevel(int curCheckpoint)
+    public static void LoadCurrentLevel(int curCheckpoint)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
