22d27e9130224b8c780f0c8cd8092f6144cddc41
ChronoE123 <33222150+ChronoE123@users.noreply.github.com>
2020-10-26 23:55:21 +0000

    Menu and Settings
    the main menu can go to setting and quit game, it has start game but function does not do anyhing yet, settings displays music, SFX, langugese and back, the back button closes the settings UI

---
+++ BingerNinja/Assets/Scripts/MainMenu_Manager_Elliott.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class MainMenu_Manager_Elliott : MonoBehaviour
+{
+
+
+    public GameObject SettingMenu;
+    protected bool m_openSettings;
+
+    public void Resume()
+    {
+
+    }
+
+    public void StartNewGame()
+    {
+        //load level 1 here
+       // SceneManager.LoadScene("Level 1");
+    }
+
+    public void OpenSettingsMenu()
+    {
+        //open up settings
+        Debug.Log("forward");
+            SettingMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        //quit application
+        Debug.Log("quit");
+        Application.Quit();
+    }
+  
+    void Start()
+    {
+        m_openSettings = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var gamepad = Mouse.current;
+        if (gamepad == null)
+            return; // No gamepad connected.
+
+        if (gamepad.leftButton.wasPressedThisFrame)
+        {
+            // 'Use' code here
+          //  Quit();
+            OpenSettingsMenu();
+        }
+       // Vector2 move = gamepad.leftStick.ReadValue();
+        // 'Move' code here
+    }
+}
---
+++ BingerNinja/Assets/Scripts/SettingsMenu_ElliottDesouza.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class SettingsMenu_ElliottDesouza : MonoBehaviour
+{
+    
+    public void IncreaseVolume()
+    {
+
+    }
+
+    public void DecreaseVolume()
+    {
+
+    }
+
+    public void IncreaseSFX()
+    {
+
+    }
+
+    public void DecreaseSFX()
+    {
+
+    }
+
+    public void ExitSettingMenu()
+    {
+        //open up settings
+        Debug.Log("back");
+        gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var gamepad = Mouse.current;
+        if (gamepad == null)
+            return; // No gamepad connected.
+
+        if (gamepad.leftButton.wasPressedThisFrame)
+        {
+            // 'Use' code here
+            //  Quit();
+            ExitSettingMenu();
+        }
+
+        // Vector2 move = gamepad.leftStick.ReadValue();
+        // 'Move' code here
+    }
+}
