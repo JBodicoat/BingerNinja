56f271e9e6be000f96c5807e6d92198137fa797b
Alanna <alanna.paxton8@hotmail.co.uk>
2020-10-27 20:42:04 +0000

    Update Weapons_AlannaPaxton.cs

--- BingerNinja/Assets/Scripts/Weapons_AlannaPaxton.cs
+++ BingerNinja/Assets/Scripts/Weapons_AlannaPaxton.cs
@@ -1,18 +1,31 @@
 ﻿using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Security.AccessControl;
 using UnityEngine;
+
+public enum WeaponType { Fugu, Squid, Rice_ball, Kobe_Beef, Sashimi };
+
 
 
 public class Weapons_AlannaPaxton : MonoBehaviour
 {
 
+    public WeaponType CurrentWeapon;
+    public GameObject Fugu;
+    public GameObject Squid;
+    public GameObject Rice_Ball;
+    public GameObject Kobe_Beef;
+    public GameObject Sashimi;
+    
     protected int rand;
     protected PlayerHealthHunger_MarioFernandes player;
     protected EffectManager_MarioFernandes Buffs;
     protected PlayerCombat_MarioFernandes HoldingItem;
     protected bool Projectile;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,75 +42,161 @@ public class Weapons_AlannaPaxton : MonoBehaviour
     {
         
     }
-    void Damage(int damage)
+    void Attack()
     {
-        if(HoldingItem.IsHoldingFood() == true)
-        {
 
-// turn to weapon.
-        }
-        else
-        {
-            // go to eating
+    }
 
+    void Eat()
+    {
+
+        if(HoldingItem.IsHoldingFood() == true)  //&& has the eat button been pressed
+        {
+            switch (CurrentWeapon)
+            {
+                case WeaponType.Rice_ball:
+                    {
+                        HealBuff_MarioFernandes heal = new HealBuff_MarioFernandes(20.0f, 2.0f, 1.0f);
+                        Buffs.AddEffect(heal);
+                    }
+                    break;
+                case WeaponType.Sashimi:
+                    {
+                        HealBuff_MarioFernandes heal = new HealBuff_MarioFernandes(30.0f, 5.0f, 1.5f);
+                        Buffs.AddEffect(heal);
+                    }
+                    break;
+                case WeaponType.Squid:
+                    {
+                        HealBuff_MarioFernandes heal = new HealBuff_MarioFernandes(15.0f, 2.0f, 1.0f);
+                        Buffs.AddEffect(heal);
+                    }
+                    break;
+                case WeaponType.Fugu:
+                    {
+                               if(rand ==0 )
+                                {
+                                    PoisionDefuff_MarioFernandes poison = new PoisionDefuff_MarioFernandes(5.0f, 5.0f, 0.5f);
+                                    Buffs.AddEffect(poison);
+                                }
+                                else
+                                {
+                                    HealBuff_MarioFernandes heal = new HealBuff_MarioFernandes(30.0f, 2.0f, 1.0f);
+                                    Buffs.AddEffect(heal);
+                                };
+                    }
+                    break;
+                case WeaponType.Kobe_Beef:
+                    {
+                        HealBuff_MarioFernandes heal = new HealBuff_MarioFernandes(50.0f, 5.0f, 1.5f);
+                        Buffs.AddEffect(heal);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
-       
+
     }
    
 }
-class Fugu : Weapons_AlannaPaxton
-{   
-     public GameObject foodFugu;
-    
-    void Poison()
+
+class RangedWeapon : Weapons_AlannaPaxton
+{
+    WeaponType CurrentWeapon;
+    private void Update()
     {
-        Projectile = false;
-        if(rand ==0 )
+        switch (CurrentWeapon)
         {
-            PoisionDefuff_MarioFernandes poison = new PoisionDefuff_MarioFernandes(5.0f, 5.0f, 0.5f);
-            Buffs.AddEffect(poison);
+            case WeaponType.Rice_ball:
+                {
+
+                }
+                break;
+            case WeaponType.Sashimi:
+                {
+
+                }
+                break;
+            case WeaponType.Squid:
+                {
+
+                }
+                break;
+
+            default:
+                break;
         }
-        else
+
+    }
+
+}
+class MeleeWeapon: Weapons_AlannaPaxton
+{
+    WeaponType CurrentWeapon;
+    private void Update()
+    {
+        switch (CurrentWeapon)
         {
-            HealBuff_MarioFernandes heal = new HealBuff_MarioFernandes(30.0f, 2.0f, 1.0f);
-            Buffs.AddEffect(heal);
-        };
- 
+            case WeaponType.Fugu:
+                {
+
+                }
+                break;
+            case WeaponType.Kobe_Beef:
+                {
+
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
-class RiceBalls : Weapons_AlannaPaxton
-{
-    public GameObject foodRiceBalls;
-    // need to get the player hunger so if eat is true to update hunger 
-    bool IsProjectile() // this is pretty much the isweapon or isfood
-    {
-       Projectile = true;
-        //get ranged weapon info
-        return false;
-    }
-    HealBuff_MarioFernandes heal1 = new HealBuff_MarioFernandes(15.0f, 2.0f, 1.0f);
+//class Fugu : Weapons_AlannaPaxton
+//{   
+//     public GameObject foodFugu;
+    
+//    void Poison()
+//    {
+//        
+ 
+//    }
+//}
+//class RiceBalls : Weapons_AlannaPaxton
+//{
+//    public GameObject foodRiceBalls;
+//    // need to get the player hunger so if eat is true to update hunger 
+//    bool IsProjectile() // this is pretty much the isweapon or isfood
+//    {
+//       Projectile = true;
+//        //get ranged weapon info
+//        return false;
+//    }
+//    HealBuff_MarioFernandes heal1 = new HealBuff_MarioFernandes(15.0f, 2.0f, 1.0f);
     
 
   
-}
+//}
 
-class KobeBeef : Weapons_AlannaPaxton
-{
+//class KobeBeef : Weapons_AlannaPaxton
+//{
 
-    //need the base speed from character code
-    // either creating a variable for boost speed or referencing another part of code
-    // how long the boost speed lasts for
+//    //need the base speed from character code
+//    // either creating a variable for boost speed or referencing another part of code
+//    // how long the boost speed lasts for
 
-    // void Newspeed() if there is no referencing 
-    HealBuff_MarioFernandes heal1 = new HealBuff_MarioFernandes(50.0f, 5.0f, 1.5f);
-}
+//    // void Newspeed() if there is no referencing 
+//    HealBuff_MarioFernandes heal1 = new HealBuff_MarioFernandes(50.0f, 5.0f, 1.5f);
+//}
 
-class Sashimi : Weapons_AlannaPaxton
-{
-    //amount of time the strength stays on for 
-    // void StrengthTimer() if there is no timer code already
-    HealBuff_MarioFernandes heal1 = new HealBuff_MarioFernandes(30.0f, 5.0f, 1.5f);
+//class Sashimi : Weapons_AlannaPaxton
+//{
+//    //amount of time the strength stays on for 
+//    // void StrengthTimer() if there is no timer code already
+//    HealBuff_MarioFernandes heal1 = new HealBuff_MarioFernandes(30.0f, 5.0f, 1.5f);
 
-}
+//}
 
 
