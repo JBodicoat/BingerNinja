382f6aacef309904c51299fe6aac6c78a562a0dd
sebastian mol <sebastianmol1996@gmail.com>
2020-10-28 18:15:22 +0000

    cook finished
    coudl still fix bugs if i find any

--- BingerNinja/Assets/Scripts/AI/BaseEnemy_SebastianMol.cs
+++ BingerNinja/Assets/Scripts/AI/BaseEnemy_SebastianMol.cs
@@ -12,7 +12,6 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
 {
     public Transform m_rayCastStart; //start position of the ray cast
     public PolygonCollider2D m_detectionCollider; // the collder cone used for player detection
-    internal Transform m_playerTransform; //used to get playe position can be null if undedteceted
     public bool m_playerDetected = false; //has the player been detected
     public enum state { WONDER, CHASE, ATTACK, RETREAT};
     public state m_currentState = state.WONDER;//current state of teh enemy
@@ -27,37 +26,42 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
     [Tooltip("amaount of damage an enemy takes")]
     public float m_DamageToTake;
     public bool showPath = false;
-
-    private Pathfinder_SebastianMol m_pathfinder;
-    internal List<Vector2Int> m_currentPath = new List<Vector2Int>();
-
     public Tilemap floortilemap;
 
+    private Pathfinder_SebastianMol m_pathfinder;
+    protected List<Vector2Int> m_currentPath = new List<Vector2Int>();
+    protected Transform m_playerTransform; //used to get playe position can be null if undedteceted
+    private float m_scale;
+    private Vector3 m_lastPos;
+
+
     abstract internal void EnemyBehaviour();
-    internal void PlayerDetection(GameObject collision) //detect player in vision cone the establishes line of sight
+    protected void PlayerDetection(GameObject collision) //detect player in vision cone the establishes line of sight
     {
         if(collision.CompareTag("Player"))
         {
-            m_detectionCollider.enabled = false;
-            RaycastHit2D hit = Physics2D.Linecast(m_rayCastStart.position, collision.transform.position);
-            Debug.DrawLine(m_rayCastStart.position, collision.transform.position, Color.red);
+            if(collision.GetComponent<PlayerStealth_JoaoBeijinho>().IsStealthed() == false)
+            {
+                m_detectionCollider.enabled = false;
+                RaycastHit2D hit = Physics2D.Linecast(m_rayCastStart.position, collision.transform.position);
+                Debug.DrawLine(m_rayCastStart.position, collision.transform.position, Color.red);
 
-            if(hit.collider.gameObject.name == "Player")
-            {
-                Debug.Log("hit player");
-                m_playerDetected = true;
-                m_playerTransform = hit.transform;
-                Debug.Log("state.attck");
-                m_currentState = state.ATTACK;
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    m_playerDetected = true;
+                    m_playerTransform = hit.transform;
+                    m_currentState = state.ATTACK;
+                    ClearPath();
+                }
+                else
+                {
+                    m_detectionCollider.enabled = true;
+                }
             }
-            else
-            {
-                m_detectionCollider.enabled = true;
-                Debug.Log("hit " + hit.collider.gameObject.name);
-            }
+
         }
-    } 
-    internal void Death() //actions that happen befor enemy death
+    }
+    protected void Death() //actions that happen befor enemy death
     {
         if(m_health <= 0)
         {
@@ -72,7 +76,7 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
             }
         }    
     }
-    internal void IsPlayerDetected()
+    protected void IsPlayerDetected()
     {
         if (m_playerDetected == true)
         {
@@ -81,7 +85,7 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
         }
     }
 
-    internal void MoveToWorldPos(Vector3 destentaion)
+    protected void MoveToWorldPos(Vector3 destentaion)
     {
         Vector2Int tileMapSpaceStart = (Vector2Int)m_pathfinder.m_tileMap.WorldToCell(transform.position);
         Vector2Int tileMapSpaceTarget = (Vector2Int)m_pathfinder.m_tileMap.WorldToCell(destentaion);
@@ -98,7 +102,7 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
         }
     }
 
-    internal void FollowPath()
+    protected void FollowPath()
     {
         if (m_currentPath.Count == 0) return;//skip check
 
@@ -130,7 +134,7 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
 
     }
 
-    internal void ClearPath() // use this to stop walking
+    protected void ClearPath() // use this to stop walking
     {
         if (showPath)
         {
@@ -143,6 +147,52 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
         m_currentPath.Add((Vector2Int)m_pathfinder.m_tileMap.WorldToCell(transform.position));
     }
 
+    protected bool IsPlayerInLineOfSight() // can you see the player
+    {
+        m_detectionCollider.enabled = false;
+        RaycastHit2D hit = Physics2D.Linecast(m_rayCastStart.position, m_playerTransform.position);
+        Debug.DrawLine(m_rayCastStart.position, m_playerTransform.position, Color.red);
+
+        if (hit.collider.gameObject.CompareTag("Player"))
+        {
+            return true;
+        }
+        else
+        {
+            m_detectionCollider.enabled = true;
+            return false;
+        }
+        
+    }
+
+    protected void SwapDirections()
+    {
+        if(m_playerDetected)
+        {
+            if(m_playerTransform.position.x > transform.position.x)
+            {
+                transform.localScale = new Vector3( -m_scale, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(m_scale, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else
+        {
+            if(m_lastPos.x > transform.position.x)
+            {
+                transform.localScale = new Vector3(m_scale, transform.localScale.y, transform.localScale.z);
+             
+            }
+            else if(m_lastPos.x < transform.position.x)
+            {
+                transform.localScale = new Vector3(-m_scale, transform.localScale.y, transform.localScale.z);
+            }
+            m_lastPos = transform.position;
+        }
+    }
+
     public void TakeDamage(float damage)
     {
         //m_health -= m_DamageToTake;
@@ -152,11 +202,15 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
     private void Start()
     {
         m_pathfinder = GameObject.FindObjectOfType<Pathfinder_SebastianMol>();
+        m_scale = transform.localScale.x;
+        m_lastPos = transform.position;
     }
+
     private void Update()
     {
         EnemyBehaviour(); // behaviour of the enemy what stste it is in and what it dose
         FollowPath();
+        SwapDirections();
         Death();//checks to see if enemy is dead 
     }
 
@@ -167,5 +221,13 @@ abstract class BaseEnemy_SebastianMol : MonoBehaviour
             PlayerDetection(collision.gameObject);
         }
        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (m_playerDetected == false)
+        {
+            PlayerDetection(collision.gameObject);
+        }
     }
 }
--- BingerNinja/Assets/Scripts/AI/RangedEnemy_SebastianMol.cs
+++ BingerNinja/Assets/Scripts/AI/RangedEnemy_SebastianMol.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.Networking;
@@ -8,47 +10,42 @@ using UnityEngine.Tilemaps;
 class RangedEnemy_SebastianMol : BaseEnemy_SebastianMol
 {
     [Header("projectile prefabs")]
-    public GameObject aimer;
-    public GameObject bullet;
+    public GameObject m_aimer;
+    public GameObject m_projectile;
     [Header("projectile Variables")]
     [Tooltip("how fast the projectile moves")]
-    public float shootDeley;
+    public float m_shootDeley;
     [Tooltip("speed of the projectile")]
-    public float projectileSpeed;
+    public float m_projectileSpeed;
     [Tooltip("how far the player can be befor enemy shoots")]
-    public float shootingRange;
-    private float timer;
-    public Vector3 pos;
-    public bool button = false;
-    public bool button2 = false;
+    public float m_shootingRange;
+    [Tooltip("how long the player has to be out of sight befor enemy losses intrest")]
+    public float m_outOfSightDeley;
+    private float m_timer;
+    private float m_timer2;
 
+    [Tooltip("point of patrole")]
+    public Vector2 m_patrolPos1;
+    [Tooltip("second point of patrole")]
+    public Vector2 m_patrolPos2;
 
+    public float m_deleyBetweenPatrol;
+    private float m_timer3;
+    private Vector2 m_currentPatrolePos;
 
     internal override void EnemyBehaviour()
     {
-        if (button)
-        {
-            MoveToWorldPos(pos);
-            button = false;
-        }
-
-        if(button2)
-        {
-            ClearPath();
-            button2 = false;
-        }
-
-
         switch (m_currentState)
         {
             case state.WONDER:
                 //move form one postion to another
                 if (m_playerDetected) m_currentState = state.CHASE;
+                Patrole();
                 break;
 
             case state.CHASE:
                 //move towards the player if he has been detected
-                if (Vector2.Distance(transform.position, m_playerTransform.position) < shootingRange/2)
+                if (Vector2.Distance(transform.position, m_playerTransform.position) < m_shootingRange / 1.5f)
                 {
                     ClearPath();
                     m_currentState = state.ATTACK;
@@ -65,45 +62,77 @@ class RangedEnemy_SebastianMol : BaseEnemy_SebastianMol
 
             case state.ATTACK:
                 //personalized attack
-                RangedAttack();
-                Debug.Log("attacking");
-                break;
-
-            case state.RETREAT:
-                //if (Vector2.Distance(transform.position, m_playerTransform.position) < shootingRange)
-                //{
-                //    m_currentState = state.ATTACK;
-                //}
-                break;
-        }     
-    }
-    private void RangedAttack()
-    {
-        if (Vector2.Distance(transform.position, m_playerTransform.position) < shootingRange)
-        {
-            Debug.Log("whitin range");
-            if (m_playerTransform != null)
-            {
-                Debug.Log("see the player");
-                Vector3 dir = Vector3.Normalize(m_playerTransform.position - transform.position);
-                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-                aimer.transform.eulerAngles = new Vector3(0, 0, angle);
-
-                if (timer <= 0)
+                if (IsPlayerInLineOfSight())
                 {
-                    GameObject projectile = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(dir.x, dir.y, 0)));
-                    projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(dir.x * projectileSpeed, dir.y * projectileSpeed);
-                    timer = shootDeley;
+                    Debug.Log("can see player");
+                    RangedAttack();
+                    m_timer2 = m_outOfSightDeley;
+                    m_playerDetected = true;
                 }
                 else
                 {
-                    timer -= Time.deltaTime;
+                    Debug.Log("cant see player");
+                    m_playerDetected = false;
+                    if (m_timer2 <= 0)
+                    {
+                        m_currentState = state.WONDER;
+                    }
+                    else
+                    {
+                        m_timer2 -= Time.deltaTime;
+                    }
+                }
+                break;
+        }     
+    }
+    private void Patrole()
+    {
+        if (m_currentPatrolePos.x == 0 && m_currentPatrolePos.y == 0) m_currentPatrolePos = m_patrolPos1;
+        if (m_currentPath.Count == 0) MoveToWorldPos(m_currentPatrolePos);
+        if (Vector2.Distance(transform.position, m_currentPatrolePos) <= 0.4f) SwapPatrolePoints();
+        FollowPath();
+    }
+
+    private void SwapPatrolePoints()
+    {
+        if (m_timer3 <= 0)
+        {
+            if (m_currentPatrolePos == m_patrolPos1)
+            {
+                m_currentPatrolePos = m_patrolPos2;
+            }
+            else
+            {
+                m_currentPatrolePos = m_patrolPos1;
+            }
+            m_timer3 = m_deleyBetweenPatrol;
+        }
+        else
+        {
+            m_timer3 -= Time.deltaTime;
+        }
+    }
+    private void RangedAttack()
+    {
+        if (Vector2.Distance(transform.position, m_playerTransform.position) < m_shootingRange)
+        {
+            if (m_playerTransform != null)
+            {
+                Vector3 dir = Vector3.Normalize(m_playerTransform.position - transform.position);
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                m_aimer.transform.eulerAngles = new Vector3(0, 0, angle);
+
+                if (m_timer <= 0)
+                {
+                    GameObject projectile = Instantiate(m_projectile, transform.position, Quaternion.Euler(new Vector3(dir.x, dir.y, 0)));
+                    projectile.GetComponent<BulletMovment_SebastianMol>().direction = (m_playerTransform.position - transform.position).normalized;
+                    m_timer = m_shootDeley;
+                }
+                else
+                {
+                    m_timer -= Time.deltaTime;
                 }
             }
-            //else
-            //{
-            //    m_currentState = state.RETREAT;
-            //}
         }
         else 
         {
@@ -114,6 +143,6 @@ class RangedEnemy_SebastianMol : BaseEnemy_SebastianMol
     void OnDrawGizmosSelected()
    {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, shootingRange);
+        Gizmos.DrawWireSphere(transform.position, m_shootingRange);
    }
 }
