e5c9c2d0cf4f68f0bbd880fda5e6b07c670eb8f4
Beijinho <60109149+Beijinho@users.noreply.github.com>
2020-10-29 14:32:39 +0000

    Door events(except Key)

---
+++ BingerNinja/Assets/Scripts/Doors/DoorButton_JoaoBeijinho.cs
@@ -0,0 +1,56 @@
+﻿//Joao Beijinho
+
+//Joao Beijinho 29/10/2020 - Created a trigger for the buttons
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is for Buttons that open doors. Either if activated by the player or hit by a projectile
+/// </summary>
+public class DoorButton_JoaoBeijinho : MonoBehaviour
+{
+    PlayerController_JamieG m_playerControllerScript;
+
+    public GameObject m_door;
+
+    private string m_playerTag = "Player";
+    private string m_projectileTag = "Projectile";
+
+    public bool m_canPressButton = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == m_playerTag)//Collision with Player
+        {
+            m_canPressButton = true;
+        }
+        else if (collision.name == m_projectileTag)//Collision with Projectile
+        {
+            m_door.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == m_playerTag)//Collision with Player
+        {
+            m_canPressButton = false;
+        }
+    }
+
+    void Awake()
+    {
+        m_playerControllerScript = FindObjectOfType<PlayerController_JamieG>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_playerControllerScript.m_interact.triggered && m_canPressButton)//Player interaction with button
+        {
+            m_door.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+}
---
+++ BingerNinja/Assets/Scripts/Doors/WeightedPressurePad_JoaoBeijinho.cs
@@ -0,0 +1,35 @@
+﻿//Joao Beijinho
+
+//Joao Beijinho 29/10/2020 - Created triggers for weighted pressure pads
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is for Weighted Pressure Pads. An object needs to stay on top of it so that the door can remain open
+/// </summary>
+public class WeightedPressurePad_JoaoBeijinho : MonoBehaviour
+{
+    public GameObject m_door;
+
+    private string m_playerTag = "Player";
+    private string m_crateTag = "Crate";
+    private string m_meleeWeaponTag = "MeleeWeapon";
+
+    private void OnTriggerEnter2D(Collider2D collision)//Open door when an Object is on top of it
+    {
+        if (collision.tag == m_playerTag || collision.tag == m_crateTag || collision.tag == m_meleeWeaponTag)//collision with every array object except Projectile
+        {
+            m_door.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+        
+    private void OnTriggerExit2D(Collider2D collision)//Close door when an object is removed from it
+    {
+        if (collision.tag == m_playerTag || collision.tag == m_crateTag || collision.tag == m_meleeWeaponTag)//if this scirpt is in a Pressure Pad
+        {
+            m_door.GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+    }
+}
