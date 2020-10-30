d9c187612e37ae58a512f4c85a7585cbe7ffad16
Beijinho <60109149+Beijinho@users.noreply.github.com>
2020-10-30 21:26:57 +0000

    Updated Inventory and PickupItem scripts, created DoorKey script

---
+++ BingerNinja/Assets/Scripts/Doors/DoorKey_JoaoBeijinho.cs
@@ -0,0 +1,30 @@
+﻿//Joao Beijinho
+
+//Joao Beijinho 30/10/2020 - Created This script
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class checks if there is a key in the inventory, if there is, it opens
+/// </summary>
+public class DoorKey_JoaoBeijinho : MonoBehaviour
+{
+    protected Inventory_JoaoBeijinho m_inventory;
+
+    private string m_playerTag = "Player";
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == m_playerTag && m_inventory.HasItem(ItemType.Key))//Collision with Player
+        {
+            gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+
+    void Start()
+    {
+        m_inventory = FindObjectOfType<Inventory_JoaoBeijinho>();    
+    }
+}
--- BingerNinja/Assets/Scripts/Inventory/Inventory_JoaoBeijinho.cs
+++ BingerNinja/Assets/Scripts/Inventory/Inventory_JoaoBeijinho.cs
@@ -1,47 +1,79 @@
 ﻿//Joao Beijinho
 
 //Joao Beijinho 26/10/2020 - Created this script along with GiveItem(), CheckForItem() and RemoveItem() functions
+//Joao Beijinho 29/10/2020 - Started using struct and enum
+//Joao Beijinho 30/10/2020 - Removed Struct and added dictionary
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+/// List of all pickuble items
+/// </summary>
+public enum ItemType
+{
+    Key,
+    type2,
+}
+
+/// <summary>
 /// This class contains a list, aka Inventory, along with functions to interact with the items
 /// </summary>
 public class Inventory_JoaoBeijinho : MonoBehaviour
 {
-    public List<Item_JoaoBeijinho> m_characterItems = new List<Item_JoaoBeijinho>();
-    public ItemDatabase_JoaoBeijinho m_itemDatabase;
+    public Dictionary<ItemType, int> m_inventoryItems = new Dictionary<ItemType, int>();
 
-    /// <summary>
-    /// Use this function to add an Item from the Item DataBase to the Inventory
-    /// </summary>
-    public void GiveItem(string itemName)
+    public void GiveItem(ItemType whatType, int howMany)
     {
-        Item_JoaoBeijinho itemToAdd = m_itemDatabase.GetItem(itemName);
-        m_characterItems.Add(itemToAdd);
-        print("Added to Inventory: " + itemToAdd.m_name);
-    }
-
-    /// <summary>
-    /// Use this function to check if an Item is in the Inventory
-    /// </summary>
-    public Item_JoaoBeijinho CheckForItem(int id)
-    {
-        return m_characterItems.Find(item => item.m_id == id);
-    }
-
-    /// <summary>
-    /// Use this function to remove an Item from the Inventory
-    /// </summary>
-    public void RemoveItem(int id)
-    {
-        Item_JoaoBeijinho item = CheckForItem(id);
-        if (item != null)
+        if (m_inventoryItems.ContainsKey(whatType))
         {
-            m_characterItems.Remove(item);
-            print("Removed from Inventory: " + item.m_name);
+
+            m_inventoryItems[whatType] += howMany;
         }
+        else
+        {
+            m_inventoryItems.Add(whatType, howMany);
+        }
+
+        foreach (KeyValuePair<ItemType, int> pair in m_inventoryItems)
+        {
+            print("You have: " + pair.Value + " " + pair.Key);
+           
+        }
+            print(howMany + " " + whatType + " added to Inventory!");
     }
+    public bool HasItem(ItemType item)
+    {
+        if (m_inventoryItems.ContainsKey(item))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    ///// <summary>
+    ///// Use this function to check if an Item is in the Inventory
+    ///// </summary>
+    //public Item_JoaoBeijinho CheckForItem(int id)
+    //{
+    //    return m_characterItems.Find(item => item.m_id == id);
+    //}
+    //
+    ///// <summary>
+    ///// Use this function to remove an Item from the Inventory
+    ///// </summary>
+    //public void RemoveItem(int id)
+    //{
+    //    Item_JoaoBeijinho item = CheckForItem(id);
+    //    if (item != null)
+    //    {
+    //        m_characterItems.Remove(item);
+    //        print("Removed from Inventory: " + item.m_name);
+    //    }
+    //}
 }
