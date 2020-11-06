1fdee181a7ea5788c0f0e6d69e0b9d730b4203b2
Beijinho <60109149+Beijinho@users.noreply.github.com>
2020-11-06 17:53:52 +0000

    Freezer for head chef boss fight

--- BingerNinja/Assets/Scripts/Control Panel/ControlPanelActivateObject_JoaoBeijinho.cs
+++ BingerNinja/Assets/Scripts/Control Panel/ControlPanelActivateObject_JoaoBeijinho.cs
@@ -1,6 +1,7 @@
 ﻿//Joao Beijinho
 
 //Joao Beijinho 05/11/2020 - Created script, enum for the type of item, a switch to activate the objects functionality and the door functionality
+//Joao Beijinho 06/11/2020 - Created freezer functionality along with FreezerLockAndDamage IEnumerator
 
 using System.Collections;
 using System.Collections.Generic;
@@ -16,9 +17,15 @@ public class ControlPanelActivateObject_JoaoBeijinho : MonoBehaviour
         Door,
         Light,
         Computer,
+        Freezer,
     }
 
     public ObjectType m_functionality;
+
+    private GameObject m_freezerBoss;//Reference enemy to do damage
+    private FreezerTrigger_JoaoBeijinho m_freezerArea;//Reference script that checks if enemy is in the freezer
+
+    public bool m_freezerDoorLock = false;//Lock/Unlock door
 
     public void ActivateObject()//Call this function to activate object functionality
     {
@@ -35,6 +42,35 @@ public class ControlPanelActivateObject_JoaoBeijinho : MonoBehaviour
                 print("This a computer");
                 //make computer sound
                 break;
+            case ObjectType.Freezer:
+                gameObject.GetComponent<Collider2D>().enabled = true;//Lock
+                m_freezerDoorLock = true;//Check if freezer door is locked
+                StartCoroutine(FreezerLockAndDamage(5, 1.5f, 3f));
+                break;
         }
+    }
+
+    IEnumerator FreezerLockAndDamage(int maxTicks, float damageInterval, float damageAmount)
+    {
+        if (m_freezerArea.m_enemyInFreezer == true)
+        {
+            for (int i = 0; i < maxTicks; i++)
+            {
+                print("Dealt " + damageAmount + " damage");
+                m_freezerBoss.GetComponent<BaseEnemy_SebastianMol>().m_health -= damageAmount;//Do damage, player for test
+                print("Enemy HP: " + m_freezerBoss.GetComponent<BaseEnemy_SebastianMol>().m_health);
+                yield return new WaitForSeconds(damageInterval);//Delay before doing damage again
+
+                i++;
+            }
+        }
+        
+        gameObject.GetComponent<Collider2D>().enabled = false;//Unlock freezer door
+    }
+
+    void Awake()
+    {
+        m_freezerBoss = GameObject.Find("ThugEnemy (1)");//Instance of ThugEnemy for testing
+        m_freezerArea = FindObjectOfType<FreezerTrigger_JoaoBeijinho>();
     }
 }
---
+++ BingerNinja/Assets/Scripts/FreezerTrigger_JoaoBeijinho.cs
@@ -0,0 +1,31 @@
+﻿//Joao Beijinho
+
+//Joao Beijinho 06/11/2020 - Created this script and trigger events
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class checks if the enemy is in trigger area, to later call in the ControlPanelActivateObject Script
+/// </summary>
+public class FreezerTrigger_JoaoBeijinho : MonoBehaviour
+{
+    public bool m_enemyInFreezer = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")//Player for testing
+        {
+            m_enemyInFreezer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")//Player for testing
+        {
+            m_enemyInFreezer = false;
+        }
+    }
+}
