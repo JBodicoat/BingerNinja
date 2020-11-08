a588a21e99b157e1d3ba0a4448abe5d9993384fa
chasewilding <60220210+chasewilding@users.noreply.github.com>
2020-11-08 18:59:27 +0000

    Added triggers and scripted death

---
+++ BingerNinja/Assets/Scripts/Level Scripting/LevelScripting.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Chase Wilding - 8/11/2020
+ * 
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelScripting : MonoBehaviour
+{
+    bool businessBossDead = false;
+    GameObject finalTrigger;
+    private void Awake()
+    {
+        finalTrigger = GameObject.Find("Post Fight");
+        finalTrigger.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(!businessBossDead)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                GameObject boss = GameObject.FindGameObjectWithTag("Enemy");
+                
+                if (boss.GetComponent<BaseEnemy_SebastianMol>().m_health <= 0)
+                {
+                    //dramatic death SE
+                    //freeze on enemy as he dies
+                    finalTrigger.SetActive(true);
+                    businessBossDead = true;
+                }
+            }
+        }
+       
+    }
+}
