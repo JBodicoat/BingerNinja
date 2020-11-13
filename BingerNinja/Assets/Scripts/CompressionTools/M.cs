587b0839de2321450225ac4b60a66cae32f8c457
StamplerJ <jann.luellmann@web.de>
2020-11-13 15:11:25 +0000

    Abstraction of MonoBehaviour

---
+++ BingerNinja/Assets/Scripts/CompressionTools/M.cs
@@ -0,0 +1,57 @@
+﻿// Jann
+
+// Jann - 13/11/20 - Abstraction of MonoBehaviour
+
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Derive from this instead of MonoBehaviour
+/// </summary>
+public class M : MonoBehaviour
+{
+    public T G<T>() where T : Component
+    {
+        return GetComponent<T>();
+    }
+
+    public T G<T>(GameObject g) where T : Component
+    {
+        return g.GetComponent<T>();
+    }
+
+    public GameObject F(string s)
+    {
+        return GameObject.Find(s);
+    }
+
+    public GameObject FT(string s)
+    {
+        return GameObject.FindGameObjectWithTag(s);
+    }
+
+    public T F<T>() where T : Object
+    {
+        return FindObjectOfType<T>();
+    }
+
+    public T[] Fs<T>() where T : Object
+    {
+        return FindObjectsOfType<T>();
+    }
+
+    public Transform C(Transform g, int i)
+    {
+        return g.GetChild(i);
+    }
+
+    public Coroutine SC(IEnumerator c)
+    {
+        return StartCoroutine(c);
+    }
+
+    public void D(GameObject g)
+    {
+        Destroy(g);
+    }
+}
