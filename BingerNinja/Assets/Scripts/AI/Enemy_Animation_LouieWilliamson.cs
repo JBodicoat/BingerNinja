b4517f3262e70f9b01afb00d95977d0dad08151b
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2020-11-18 13:22:11 +0000

    EnemyAnimations

---
+++ BingerNinja/Assets/Scripts/AI/Enemy_Animation_LouieWilliamson.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Animation_LouieWilliamson : MonoBehaviour
+{
+    private Animator m_animator;
+    private Vector2 m_lastPosition;
+    private float m_xMovement;
+    private float m_yMovement;
+    private SpriteRenderer m_spriteRenderer;
+    private bool isFacingLeft;
+
+    public void AttackAnimation()
+    {
+        m_animator.SetTrigger("isAttacking");
+    }
+    private void Start()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+        m_lastPosition = transform.position;
+        m_spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        isFacingLeft = true;
+    }
+
+    private void Update()
+    {
+        m_xMovement = m_lastPosition.x - transform.position.x;
+        m_yMovement = m_lastPosition.y - transform.position.y;
+        
+        //face enemy in the right direction
+        if (m_yMovement > 0)
+        {
+            m_animator.SetBool("isFacing", true);
+            print("NOT FACING");
+        }
+        else if (m_yMovement < 0)
+        {
+            m_animator.SetBool("isFacing", false);
+            print("FACING");
+        }
+
+        //Is he moving to the left
+        if (m_xMovement < 0)
+        {
+            //is he already facing left?
+            if (!isFacingLeft)
+            {
+                FlipSprite();
+                isFacingLeft = true;
+            }
+        }
+        else if (m_xMovement > 0)
+        {
+            if (isFacingLeft)
+            {
+                FlipSprite();
+                isFacingLeft = false;
+            }
+        }
+
+        //Set Idle/ Moving animation trigger
+        if (m_xMovement == 0 && m_yMovement == 0)
+        {
+            m_animator.SetTrigger("isIdle");
+            print("IDLE");
+        }
+        else
+        {
+            m_animator.SetTrigger("isMoving");
+            print("MOVE");
+        }
+
+        
+
+        m_lastPosition = transform.position;
+    }
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        print("FLIP");
+    }
+}
