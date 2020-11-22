e36591d49b96410663da7f469e1df2802be21958
StamplerJ <jann.luellmann@web.de>
2020-11-22 15:46:47 +0000

    Added notes (C2 - C3)

--- BingerNinja/Assets/Scripts/Audio/NotesCreator_Jann.cs
+++ BingerNinja/Assets/Scripts/Audio/NotesCreator_Jann.cs
@@ -7,6 +7,7 @@
 // Jann 25/10/20 - Refactored parts to PlayTrack_Jann.cs
 // Jann 28/10/20 - QA improvements
 // Jann 11/11/20 - Added GetNotes(frequency)
+// Jann 22/11/20 - Added more notes (C2 - C3)
 
 using System;
 using System.Collections.Generic;
@@ -16,7 +17,7 @@ using UnityEngine;
 public class NotesCreator_Jann
 {
     private const int BaseFrequency = 440;
-    private const int From = -21;
+    private const int From = -33;
     private const int To = 15;
     private const float FrequencyRation = 1.059463094359f;
 
@@ -31,6 +32,7 @@ public class NotesCreator_Jann
     public enum Note
     {
         None = 0, 
+        C2, Db2, D2, Eb2, E2, F2, Gb2, G2, Ab2, A2, Bb2, B2, 
         C3, Db3, D3, Eb3, E3, F3, Gb3, G3, Ab3, A3, Bb3, B3, 
         C4, Db4, D4, Eb4, E4, F4, Gb4, G4, Ab4, A4, Bb4, B4, 
         C5, Db5, D5, Eb5, E5, F5, Gb5, G5, Ab5, A5, Bb5, B5, 
