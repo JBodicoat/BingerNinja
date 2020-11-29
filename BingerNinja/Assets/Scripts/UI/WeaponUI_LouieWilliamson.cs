bbfb671b54c346f558d3daaf28db46bd315ffc10
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2020-11-29 13:13:26 +0000

    Revert "Weapon UI animation update"
    This reverts commit 15c52c2cdfc833b79eac98c8949a020f58e57c49.

--- BingerNinja/Assets/Scripts/UI/WeaponUI_LouieWilliamson.cs
+++ BingerNinja/Assets/Scripts/UI/WeaponUI_LouieWilliamson.cs
@@ -26,6 +26,9 @@ public class WeaponUI_LouieWilliamson : MonoBehaviour
     private Animator weaponsAnim;
     public Animator pickupAnim;
     public Animator keyAnim;
+
+    private float timer;
+
     public List<Sprite> WeaponSprites = new List<Sprite>();
 
     //---- LIST KEY ----   //
@@ -51,9 +54,9 @@ public class WeaponUI_LouieWilliamson : MonoBehaviour
     {
         pickupAnim.SetBool("isPickupShown", hasPickup);
     }
-    public void setPickupImage(FoodType newPickup)
+    public void setPickupImage()
     {
-        pickupImage.sprite = WeaponSprites[(int)newPickup];
+        //pickupImage.sprite = ;
     }
 
     public void WeaponChange(FoodType newWeapon, bool isRanged, int ammo)
@@ -62,12 +65,78 @@ public class WeaponUI_LouieWilliamson : MonoBehaviour
         {
             setAmmo("" + ammo);
         }
+
         setName(newWeapon.ToString(), isRanged);
         setImage((int)newWeapon, isRanged);
+
+        //switch (newWeapon)
+        //{
+        //    case FoodType.FUGU:
+        //        setName("Fugu", isRanged);
+        //        setImage( 0, isRanged);
+        //        break;
+        //    case FoodType.SQUID:
+        //        setName("Squid", isRanged);
+        //        setImage(1, isRanged);
+        //        break;
+        //    case FoodType.RICEBALL:
+        //        setName("Riceball", isRanged);
+        //        setImage(2, isRanged);
+        //        break;
+        //    case FoodType.KOBEBEEF:
+        //        setName("Kobe Beef", isRanged);
+        //        setImage(3, isRanged);
+        //        break;
+        //    case FoodType.SASHIMI:
+        //        setName("Sashimi", isRanged);
+        //        setImage(4, isRanged);
+        //        break;
+        //    case FoodType.TEMPURA:
+        //        setName("Pizza", isRanged);
+        //        setImage(5, isRanged);
+        //        break;
+        //    case FoodType.SAKE:
+        //        setName("Sake", isRanged);
+        //        setImage(6, isRanged);
+        //        break;
+        //    case FoodType.NOODLES:
+        //        setName("Noodles", isRanged);
+        //        setImage(7, isRanged);
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
     void Start()
     {
         weaponsAnim = GetComponent<Animator>();
+
+        timer = 0;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 25)
+        {
+            WeaponChange(FoodType.FUGU, false, 0);
+        }
+        else if (timer > 20)
+        {
+            WeaponChange(FoodType.KOBEBEEF, false, 0);
+        }
+        else if (timer > 15)
+        {
+            WeaponChange(FoodType.NOODLES, false, 0);
+        }
+        else if (timer > 10)
+        {
+            WeaponChange(FoodType.RICEBALL, true, 10);
+        }
+        else if (timer > 5)
+        {
+            WeaponChange(FoodType.SQUID, false, 0);
+        }
     }
     void setName(string name, bool isRanged)
     {
@@ -95,4 +164,6 @@ public class WeaponUI_LouieWilliamson : MonoBehaviour
     {
         ammoText.text = ammo;
     }
+
+
 }
