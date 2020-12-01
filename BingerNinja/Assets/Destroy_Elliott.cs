982deaee43912c9700158df661c0254bf4ce90b4
elliott <e.desouza1996@hotmail.com>
2020-12-01 00:26:52 +0000

    update
    made an eye appaer over the players head when he his seen and made it disapaer when the enemy loses sight,

---
+++ BingerNinja/Assets/Destroy_Elliott.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Elliott : MonoBehaviour
+{
+
+    public float m_lifeTime= .1f; //This implies a delay of 2 seconds.
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, m_lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
