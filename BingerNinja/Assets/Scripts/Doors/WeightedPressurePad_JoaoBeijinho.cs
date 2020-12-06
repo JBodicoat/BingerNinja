a91248ed9a791b65bc1a3e058e88176d1272bfba
Beijinho <60109149+Beijinho@users.noreply.github.com>
2020-12-06 18:11:23 +0000

    Lvl17 pressure pad broken
    I just added  the //Door 2 section to the WeichtedPressurePad_JoaoBeijinho() script, in which I turn off the door tiles for the second door.
    
    Also assigned bools for each door of level 17 in the script, and enabled them on each pressure pad of the level in their respective inspector

--- BingerNinja/Assets/Scripts/Doors/WeightedPressurePad_JoaoBeijinho.cs
+++ BingerNinja/Assets/Scripts/Doors/WeightedPressurePad_JoaoBeijinho.cs
@@ -2,6 +2,7 @@
 
 //Joao Beijinho 29/10/2020 - Created triggers for weighted pressure pads
 //Joao Beijinho 02/11/2020 - Replaced m_door GameObject with m_doorCollider Collider2D
+//Joao Beijinho 06/12/2020 - Added //Door 2 section and bools for individual doors of Level 17
 
 using System.Collections;
 using System.Collections.Generic;
@@ -17,6 +18,7 @@ public class WeightedPressurePad_JoaoBeijinho : MonoBehaviour
     //Reference the door collider to turn if on/off(closed/open)
     public Collider2D m_doorCollider;
     public Sprite activatedPressurePad, inactivePressurePad;
+    public bool m_door1, m_door2;
     private string m_playerTag = "Player";
     private string m_crateTag = "Crate";
     private string m_meleeWeaponTag = "MeleeWeapon";
@@ -35,16 +37,26 @@ public class WeightedPressurePad_JoaoBeijinho : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().sprite = activatedPressurePad;
             if(SceneManager.GetActiveScene().buildIndex == 17)
             {
-                walls1.SetTile(new Vector3Int(32, 19, 0), null);
-                walls1.SetTile(new Vector3Int(32, 20, 0), null);
-                walls1.SetTile(new Vector3Int(32, 21, 0), null);
-                walls1.SetTile(new Vector3Int(32, 22, 0), null);
+                if (m_door1 == true)//Door 1
+                {
+                    walls1.SetTile(new Vector3Int(32, 19, 0), null);
+                    walls1.SetTile(new Vector3Int(32, 20, 0), null);
+                    walls1.SetTile(new Vector3Int(32, 21, 0), null);
+                    walls1.SetTile(new Vector3Int(32, 22, 0), null);
 
-                walls2.SetTile(new Vector3Int(33, 20, 0), null);
-                walls2.SetTile(new Vector3Int(33, 21, 0), null);
-                walls2.SetTile(new Vector3Int(33, 22, 0), null);
-                walls2.SetTile(new Vector3Int(33, 23, 0), null);
+                    walls2.SetTile(new Vector3Int(33, 20, 0), null);
+                    walls2.SetTile(new Vector3Int(33, 21, 0), null);
+                    walls2.SetTile(new Vector3Int(33, 22, 0), null);
+                    walls2.SetTile(new Vector3Int(33, 23, 0), null);
+                }
+                else if (m_door2 == true)//Door 2
+                {
+                    walls1.SetTile(new Vector3Int(24, 32, 0), null);
+                    walls1.SetTile(new Vector3Int(24, 33, 0), null);
 
+                    walls2.SetTile(new Vector3Int(25, 33, 0), null);
+                    walls2.SetTile(new Vector3Int(25, 34, 0), null);
+                }
             }
             if (SceneManager.GetActiveScene().buildIndex == 14)
             {
