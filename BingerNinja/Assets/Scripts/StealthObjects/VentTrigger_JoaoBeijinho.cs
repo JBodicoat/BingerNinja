418f4a97b70bcc01072ed127f2c04a7da47c9659
Beijinho <60109149+Beijinho@users.noreply.github.com>
2020-12-12 00:22:34 +0000

    Lvl 13 - Vent limits cage movement when not in it
    Did the same as in level 5.
    
    Changed the Vent_JoaoBeijinho script to work on the ventPath instead of the vent entrance and exit.
    
    The VentExit() is now on OnTriggerEnter2D.
    
    Created VentTrigger GameObjects and VentTrigger_JoaoBeijinho script.
    
    This script Toggles the VentPath GameObject ON/OFF

---
+++ BingerNinja/Assets/Scripts/StealthObjects/VentTrigger_JoaoBeijinho.cs
@@ -0,0 +1,29 @@
+﻿//Joao Beijinho 11/12/2020 - Created script
+//                           Created reference to ventPath GameObject
+//                           Created OnTriggerEnter2D which toggles ventPath ON/OFF
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is to be used on the vents triggers, which will activate the VentPath
+/// </summary>
+public class VentTrigger_JoaoBeijinho : MonoBehaviour
+{
+    private GameObject m_ventPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ventPath = GameObject.Find("VentPath");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)//Enter and Exit vent
+    {
+        if (collision.gameObject.CompareTag(Tags_JoaoBeijinho.m_playerTag))
+        {
+            m_ventPath.SetActive(!m_ventPath.activeInHierarchy);
+        }
+    }
+}
