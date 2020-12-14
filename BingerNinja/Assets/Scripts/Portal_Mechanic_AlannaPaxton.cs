b6dfc92b77f9864a5578d905e226ddf59312f6da
jonthebluecow <45598462+jonthebluecow@users.noreply.github.com>
2020-12-14 17:43:56 +0000

    STUFF

--- BingerNinja/Assets/Scripts/Portal_Mechanic_AlannaPaxton.cs
+++ BingerNinja/Assets/Scripts/Portal_Mechanic_AlannaPaxton.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 // alanna 09/11/20, needs to be added onto player object, then create portals and tag them appropriately. Also add animation if wanted.
 //alanna 19/11/20 this code just needs to be added onto player. when making portals in hierarchy pair the portals up so they have 1 parent and one child, then this code should work for all of them.
+//Jon did a thing
 
 public class Portal_Mechanic_AlannaPaxton : MonoBehaviour
 {
     bool canTeleport = true;
-    protected int trigger = 0;
+    public float portalDelay = 0.8f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,33 +21,26 @@ public class Portal_Mechanic_AlannaPaxton : MonoBehaviour
                 // Portal();
                 if (collision.transform.childCount > 0)
                 {
-                    SceneManager_JamieG.Instance.FadeBoth();
-                    
                     GameObject ChildGameObject1 = collision.transform.GetChild(0).gameObject;
                     Vector2 child_pos = ChildGameObject1.transform.position;
                     this.transform.position = child_pos;
-                    canTeleport = false;
+                    StartCoroutine(PortalExitDelay());
                 } else if (collision.transform.parent != null)
                 {
                     Vector2 parent_pos = collision.transform.parent.position;
                     this.transform.position = parent_pos;
-                    canTeleport = false;
+                    StartCoroutine(PortalExitDelay());
                 }
             }
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    IEnumerator PortalExitDelay()
     {
-        if(collision.gameObject.tag == "Portal")
-        {
-            if (trigger++ >= 2)
-            {
-                Debug.Log(trigger);
-                canTeleport = true;
-                trigger = 0;
-            }
-        }
-        
+        canTeleport = false;
+        SceneManager_JamieG.Instance.FadeBoth();
+        yield return new WaitForSeconds(portalDelay);
+        canTeleport = true;
     }
+
 }
